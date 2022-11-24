57e05d8e493764ff3bb3d04c1730cb61e62f4116
Vitaliy Parkhomenko <parkhomenko.wd@gmail.com>
2022-11-24 15:00:34 +0200

    Implemented context api

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 'use strict';
 
-/**
- * Core or JS implementation of reading files */
 // Types or folder names
 const TYPE_JSON = "json-files";
 const TYPE_JS = "js-files";
 
-function createApi() {
+// Persistence
+const STORAGE_KEY = "records";
+const INITIAL = {
+  [TYPE_JS]: {},
+  [TYPE_JSON]: {}
+};
+
+/**
+ * Core or JS implementation of reading files */
+const read = (() => {
   const state = {
     resourcesHandle: null,
     lastRead: 0,
@@ -117,17 +124,177 @@ function createApi() {
 
     return fileName;
   }
+})();
+
+/**
+ * Core or JS implementation of save records */
+const records = (() => {
+  const state = {
+    data: {},
+    sub: [],
+  };
+  const _notify = () => {
+    for (const notify of state.sub) {
+      notify();
+    }
+  };
+  const _update = (data) => {
+    for (const type in data) {
+      const ref = data[type];
+
+      if (ref != null && typeof ref === "object") {
+        const cur = state.data[type];
+
+        state.data[type] = cur == null ? ref : { ...cur, ...ref };
+      } else {
+        state.data[type] = ref;
+      }
+    }
+
+    return state.data;
+  };
+  const api = (type, id) => {
+    if (!state.data) {
+      return null;
+    }
+
+    if (!state.data[type]) {
+      return null;
+    }
+
+    if (Boolean(type) && id == null) {
+      return state.data[type];
+    }
+
+    if (!state.data[type][id]) {
+      return null;
+    }
+
+    return state.data[type][id];
+  };
+
+  api.subscribe = (listener) => {
+    let subscribed = true;
+
+    state.sub.push(listener);
+
+    return () => {
+      if (subscribed) {
+        subscribed = false;
+        state.sub.splice(state.sub.indexOf(listener), 1);
+      }
+    };
+  };
+
+  api.update = (data) => {
+    save(_update(data));
+  };
+
+  api.change = (data) => {
+    api.update(data);
+
+    _notify();
+  };
+
+  api.init = () => {
+    state.data = load();
+
+    _notify();
+  };
+
+  return api;
+
+  function save(data) {
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(data));
+  }
+
+  function load() {
+    const raw = localStorage.getItem(STORAGE_KEY);
+
+    return raw ? parse(raw) : INITIAL;
+  }
+
+  function parse(data) {
+    try {
+      return JSON.parse(data);
+    } catch (e) {
+      return INITIAL;
+    }
+  }
+})();
+
+/**
+ * React implementation of save records and provide data to component */
+const RecordsContext = React.createContext({});
+
+function RecordsProvider(props) {
+  React.useEffect(() => {
+    records.init();
+  }, []);
+
+  return (
+    <RecordsContext.Provider value={records}>
+      {props.children}
+    </RecordsContext.Provider>
+  );
+}
+
+function useRecords() {
+  const records = React.useContext(RecordsContext);
+  const [, set] = React.useState(0);
+
+  React.useEffect(() => records.subscribe(() => set((n) => n + 1)), []);
+
+  return records;
+}
+
+function withRecords(C) {
+  function Records(props) {
+    const records = React.useContext(RecordsContext);
+    const [_rev, set] = React.useState(0);
+
+    React.useEffect(() => records.subscribe(() => set((n) => n + 1)), []);
+
+    // Pass "_rev" prop for re-render the component
+    // For ex. in case of using with redux "connect"
+    // "Connect" wrap component into "React.memo",
+    //  the component will not update if "records" data had been changed
+    //  because "records" is the same object
+    return React.createElement(C, { ...props, records, _rev });
+  }
+
+  Records.displayName = `withRecords(${C.displayName || C.name})`;
+
+  Records.WrappedComponent = C;
+
+  return Records;
 }
 
 ReactDOM
   .createRoot(document.querySelector('#output'))
-  .render(<Page />);
+  .render(
+    <RecordsProvider>
+      <Page />
+    </RecordsProvider>
+  );
+
+const RecordItemWithHOC = withRecords(RecordItemWithHOCPure);
 
 function Page() {
   return (
     <div>
       <Title />
       <ReadFiles />
+      <Hr />
+      <h2>Usage with hook</h2>
+      <RecordItemWithHook type={TYPE_JS} id="function-1" />
+      <Hr />
+      <RecordItemWithHook type={TYPE_JSON} id="data-1" />
+      <Hr />
+      <h2>Usage with HOC</h2>
+      <RecordItemWithHOC type={TYPE_JS} id="function-2" />
+      <Hr />
+      <RecordItemWithHOC type={TYPE_JSON} id="data-2" />
     </div>
   );
 }
@@ -146,14 +313,21 @@ function Title() {
   );
 }
 
-const read = createApi();
-
 function ReadFiles() {
-  const [state, setState] = React.useState({ [TYPE_JS]: {}, [TYPE_JSON]: {} });
+  // Usage with react state
+  // const [state, setState] = React.useState({ [TYPE_JS]: {}, [TYPE_JSON]: {} });
+  // const handleClick = async () => {
+  //   const res = await read();
+  //
+  //   setState(() => res);
+  // };
+
+  // Usage with react context
+  const records = useRecords();
   const handleClick = async () => {
     const res = await read();
 
-    setState(() => res);
+    records.change(res);
   };
 
   return (
@@ -163,23 +337,54 @@ function ReadFiles() {
       <p><b>Note 1: </b><mark>resources</mark> and <mark>src</mark> is not required names, you can use names which is needed for solving your issues, just rewrite code according to your needs</p>
       <p><b>Note 2: </b>after reloading page in browser you need to open folder again to have access to resources</p>
       <button type="button" onClick={handleClick}>Click</button>
+      <Hr />
       <div>
         <h3>
           JS files
         </h3>
         <pre>
-          {JSON.stringify(state[TYPE_JS], undefined, 2)}
+          {/*{JSON.stringify(state[TYPE_JS], undefined, 2)}*/}
+          {JSON.stringify(records(TYPE_JS), undefined, 2)}
         </pre>
       </div>
-      <div style={{ backgroundColor: "gray", height: "1px", marginTop: "20px", marginBottom: "20px" }} />
+      <Hr />
       <div>
         <h3>
           JSON files
         </h3>
         <pre>
-          {JSON.stringify(state[TYPE_JSON], undefined, 2)}
+          {/*{JSON.stringify(state[TYPE_JSON], undefined, 2)}*/}
+          {JSON.stringify(records(TYPE_JSON), undefined, 2)}
         </pre>
       </div>
     </div>
   );
+}
+
+function RecordItemWithHook(props) {
+  const records = useRecords();
+
+  return (
+    <div>
+      <h3>Folder {props.type}, file: {props.id}</h3>
+      <pre>
+        {JSON.stringify(records(props.type, props.id), undefined, 2)}
+      </pre>
+    </div>
+  );
+}
+
+function RecordItemWithHOCPure(props) {
+  return (
+    <div>
+      <h3>Folder {props.type}, file: {props.id}</h3>
+      <pre>
+        {JSON.stringify(props.records(props.type, props.id), undefined, 2)}
+      </pre>
+    </div>
+  );
+}
+
+function Hr() {
+  return <div style={{ backgroundColor: "gray", height: "1px", marginTop: "20px", marginBottom: "20px" }} />;
 }
